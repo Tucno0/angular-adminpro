f938cb5571a9611ea1a60d264b13fb61f73b7434
Jhampier Tucno <yantucnoperez@gmail.com>
2023-07-01 17:12:02 -0500

    Seccion 8: Observables y Promesas

--- src/app/shared/components/bread-crumbs/bread-crumbs.component.ts
+++ src/app/shared/components/bread-crumbs/bread-crumbs.component.ts
@@ -1,29 +1,51 @@
-import { Component, inject } from '@angular/core';
-import { ActivationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { Component, OnDestroy, inject } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription, filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'shared-bread-crumbs',
   templateUrl: './bread-crumbs.component.html',
   styles: [],
 })
-export class BreadCrumbsComponent {
+export class BreadCrumbsComponent implements OnDestroy {
   private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
 
-  public titulo = undefined;
+  public titulo!: string;
+  public tituloSubs$!: Subscription;
 
   constructor() {
-    this.router.events
-      .pipe(
-        filter((e) => e instanceof ActivationEnd && e.snapshot.data['titulo']),
-        // map((e: ActivationEnd) => {
-        //   const event = e as ActivationEnd;
-        //   return event.snapshot.data;
-        // })
-      )
-      .subscribe((data) => {
-        console.log(data);
-        // this.titulo = data;
-      });
+
+    console.log(this.route.snapshot.children[0].data['titulo']);
+
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe((titulo) => {
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+      console.log(titulo);
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta() {
+    return this.router.events.pipe(
+      map((e) => {
+        if (e instanceof ActivationEnd && e.snapshot.firstChild === null) {
+          return e.snapshot.data['titulo'];
+        }
+        return 'No hay titulo';
+      }),
+      filter((e) => !(e === 'No hay titulo'))
+      // tap( (e) => (e.snapshot.data['titulo'] === undefined) ? 'No hay titulo' : e ),
+      // filter( (e) => e !== undefined )
+
+      // map( (e: Event) => {
+      //   const event = e as ActivationEnd;
+      //   return event.snapshot.data;
+      // })
+    );
   }
 }
